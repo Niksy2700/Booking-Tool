dca31f66ecb2d3b9268f705dbacbc8a672766d3f
Nikhil Sunny (MS/EBV21-VM) <ifu1cob@bosch.com>
2025-07-22 12:03:35 +0530

    Changed the port

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def book_room():
     return render_template('index.html', bookings=bookings)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
